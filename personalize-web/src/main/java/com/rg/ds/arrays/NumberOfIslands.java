da7e506ff9c1e6d81ed3e7233c7a378f46a93373
gorleramana <gorleramana@gmail.com>
2025-12-24 21:53:17 -0600

    updated UI, more user friendly

---
+++ personalize-web/src/main/java/com/rg/ds/arrays/NumberOfIslands.java
@@ -0,0 +1,60 @@
+package com.rg.ds.arrays;
+
+/**
+ * @author gorle
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+ *
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ */
+public class NumberOfIslands {
+
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+
+        NumberOfIslands solution = new NumberOfIslands();
+        int numberOfIslands = solution.numIslands(grid);
+        System.out.println("Number of islands: " + numberOfIslands); // Output: 3
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int numIslands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+    public void dfs(char[][] grid, int i, int j) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= rows || j >= cols || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0'; // Mark as visited
+
+        dfs(grid, i + 1, j); // Down
+        dfs(grid, i - 1, j); // Up
+        dfs(grid, i, j + 1); // Right
+        dfs(grid, i, j - 1); // Left
+    }
+}
